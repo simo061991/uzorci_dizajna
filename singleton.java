3c53ca980eece2959b407dce8a927512d577ada4
Šimo Matijević <simo.matijevic@gmail.com>
2018-11-06 02:31:06 +0100

    Update singleton.java

--- singleton.java
+++ singleton.java
@@ -1,12 +1,12 @@
 // Prva klasa 'suprotnoSingleton' prikazuje način rada koji se razlikuje od Singleton principa, to jest stvara više instanci
 // Druga klasa 'singletonPrimjer' objašnjava princip rada Singleton design pattern-a 
 // Treca klasa 'Singleton' sadrži main metodu te omogućava instanciranje objekata prethodne dvije klase
-// Rezultat je : a) jedna instanca iz klase 'singletonPrimjer' i b) više objekata iz klase 'suprotnoSingleton'
+// Rezultat je : a) jedna instanca iz klase 'singletonPrimjer' i b) više instanci iz klase 'suprotnoSingleton'
 
 
 
 class suprotnoSingleton{
-	 static suprotnoSingleton instanca = null;
+	static suprotnoSingleton instanca = null;
 	public static suprotnoSingleton dohvatanjeInstance() {
 			instanca = new suprotnoSingleton();			
 		return instanca;
