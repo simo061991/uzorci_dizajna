a31429aca39cb5669ac78175c0a8d870cea9412a
Šimo Matijević <simo.matijevic@gmail.com>
2018-11-07 02:54:37 +0100

    Update mainSingleton.java

--- singletonPattern/mainSingleton.java
+++ singletonPattern/mainSingleton.java
@@ -1,1 +1,15 @@
+public class mainSingleton {
+	public static void main(String[] args) {
+		singletonPrimjer instanca1 = singletonPrimjer.dohvatanjeInstance();
+		singletonPrimjer instanca2 = singletonPrimjer.dohvatanjeInstance();
+		suprotnoSingleton instanca3 = suprotnoSingleton.dohvatanjeInstance();
+		suprotnoSingleton instanca4 = suprotnoSingleton.dohvatanjeInstance();
+		
+		System.out.println(instanca1.dohvatanjeInstance());
+		System.out.println(instanca2.dohvatanjeInstance());
+		System.out.println(instanca3.dohvatanjeInstance());
+		System.out.println(instanca4.dohvatanjeInstance());
+	}
+}
+
 
